b28cebda62808b9daaa24387b524c60d550a673b
HELLBOY3110 <appucr3110@gmail.com>
2023-06-16 02:02:33 +0530

    aravinthan

--- student/timetable.php
+++ student/timetable.php
@@ -62,17 +62,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['roomNumber'])) {
             echo "<td>" . $days[$i-1] . "</td>";
 
             for ($j = 1; $j <= 8; $j++) {
-<<<<<<< Updated upstream
-                if (isset($timetable[$i][$j])) {
-                    echo "<td>" . $timetable[$i][$j] . "</td>";
-                } else {
-                    echo "<td></td>";
-                }
-            }                      
-=======
                 echo "<td>" . $timetable[$i][$j] . "</td>";
             }
->>>>>>> Stashed changes
 
             echo "</tr>";
         }
